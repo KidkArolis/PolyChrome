9b5ae14cd4abfbd92057a3e7eee4916bba53cb03
Karolis <karolis.n@gmail.com>
2011-01-10 17:14:58 +0000

    Fix: kill.sh script updated to work with ps of both Linux and MacOS. Linux ps throws a warning, but that's ok.

--- polymlext/poly/bin/kill.sh
+++ polymlext/poly/bin/kill.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # This is process id, parameter passed by user
 ppid=$1
-ps -o pid= --ppid $ppid | xargs kill -2 >/dev/null
+ps -o pid,ppid -ax | awk "{ if (\$2 == $ppid) { print \$1 }}" | xargs kill -2 >/dev/null
